03236f290a7b1de7934b03e45d13ea0633748f38
abuchanan-figment <andrew.buchanan@figment.io>
2025-12-16 10:44:07 -0500

    Update andrewtest.rb

--- Formula/andrewtest.rb
+++ Formula/andrewtest.rb
@@ -9,7 +9,9 @@ class Andrewtest < Formula
         bin.install Dir["*"]
         Dir["#{bin}/*"].each do |f|
         system "codesign", "--force", "--sign", "-", f if File.file?(f)
-        system "/usr/local/bin/docker-compose up -d"
+        cd pkgshare do
+        system "/usr/local/bin/docker-compose", "up", "-d"
+        end
         end
         
     
